d9e7f0377605194b0369fcfd5f4fd98fe4c03b89
masa-aa <60830652+masa-aa@users.noreply.github.com>
2020-05-08 00:20:14 +0900

    a

--- 教師なし/2章/7_機械学習モデル/3_LightGBM/p54_モデルの訓練.py
+++ 教師なし/2章/7_機械学習モデル/3_LightGBM/p54_モデルの訓練.py
@@ -13,14 +13,14 @@ import matplotlib as mpl
 # %matplotlib inline
 
 '''Data Prep'''
-from sklearn import preprocessing as pp 
-from scipy.stats import pearsonr 
+from sklearn import preprocessing as pp
+from scipy.stats import pearsonr
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold 
-from sklearn.metrics import log_loss 
-from sklearn.metrics import precision_recall_curve, average_precision_score 
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_recall_curve, average_precision_score
 from sklearn.metrics import roc_curve, auc, roc_auc_score
-from sklearn.metrics import confusion_matrix, classification_report 
+from sklearn.metrics import confusion_matrix, classification_report
 
 '''Algos'''
 from sklearn.linear_model import LogisticRegression
@@ -39,16 +39,16 @@ data = pd.read_csv(current_path + file)
 
 # 標準化
 dataX = data.copy().drop(['Class'],axis=1)
-dataY = data['Class'].copy() 
+dataY = data['Class'].copy()
 featuresToScale = dataX.drop(['Time'],axis=1).columns
-sX = pp.StandardScaler(copy=True, with_mean=True, with_std=True) 
+sX = pp.StandardScaler(copy=True, with_mean=True, with_std=True)
 dataX.loc[:,featuresToScale] = sX.fit_transform(dataX[featuresToScale])
 
 # 訓練セットの作成
 X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.33, random_state=2018, stratify=dataY)
 
 # k分割交差検証セットの作成
-k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018) 
+k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
 
 
 # ハイパーパラメータの設定 全てデフォルト
@@ -73,36 +73,28 @@ params_lightGB = {
 }
 #------------------------------------------------------------------------------------------
 
+# モデルの訓練
 trainingScores = []
 cvScores = []
-predictionsBasedOnKFolds = pd.DataFrame(data=[],
-                                index=y_train.index,columns=['prediction'])
+predictionsBasedOnKFolds = pd.DataFrame(data=[],index=y_train.index,columns=['prediction'])
 
-for train_index, cv_index in k_fold.split(np.zeros(len(X_train)),
-                                          y_train.ravel()):
-    X_train_fold, X_cv_fold = X_train.iloc[train_index,:], \
-        X_train.iloc[cv_index,:]
-    y_train_fold, y_cv_fold = y_train.iloc[train_index], \
-        y_train.iloc[cv_index]
-    
+for train_index, cv_index in k_fold.split(np.zeros(len(X_train)),y_train.ravel()):
+    X_train_fold, X_cv_fold = X_train.iloc[train_index,:], X_train.iloc[cv_index,:]
+    y_train_fold, y_cv_fold = y_train.iloc[train_index], y_train.iloc[cv_index]
+
     lgb_train = lgb.Dataset(X_train_fold, y_train_fold)
     lgb_eval = lgb.Dataset(X_cv_fold, y_cv_fold, reference=lgb_train)
-    gbm = lgb.train(params_lightGB, lgb_train, num_boost_round=2000,
-                   valid_sets=lgb_eval, early_stopping_rounds=200)
-    
-    loglossTraining = log_loss(y_train_fold, \
-                gbm.predict(X_train_fold, num_iteration=gbm.best_iteration))
+    gbm = lgb.train(params_lightGB, lgb_train, num_boost_round=2000,valid_sets=lgb_eval, early_stopping_rounds=200)
+
+    loglossTraining = log_loss(y_train_fold, gbm.predict(X_train_fold, num_iteration=gbm.best_iteration))
     trainingScores.append(loglossTraining)
-    
-    predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'] = \
-        gbm.predict(X_cv_fold, num_iteration=gbm.best_iteration) 
-    loglossCV = log_loss(y_cv_fold, \
-        predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'])
+
+    predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'] = gbm.predict(X_cv_fold, num_iteration=gbm.best_iteration)
+    loglossCV = log_loss(y_cv_fold, predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'])
     cvScores.append(loglossCV)
-    
+
     print('Training Log Loss: ', loglossTraining)
     print('CV Log Loss: ', loglossCV)
-    
-loglossLightGBMGradientBoosting = \
-    log_loss(y_train, predictionsBasedOnKFolds.loc[:,'prediction'])
+
+loglossLightGBMGradientBoosting = log_loss(y_train, predictionsBasedOnKFolds.loc[:,'prediction'])
 print('LightGBM Gradient Boosting Log Loss: ', loglossLightGBMGradientBoosting)
--- 教師なし/2章/7_機械学習モデル/3_LightGBM/p55_結果の評価.py
+++ 教師なし/2章/7_機械学習モデル/3_LightGBM/p55_結果の評価.py
@@ -1,3 +1,5 @@
+import time
+s=time.time()
 '''Main'''
 import numpy as np
 import pandas as pd
@@ -13,14 +15,14 @@ import matplotlib as mpl
 # %matplotlib inline
 
 '''Data Prep'''
-from sklearn import preprocessing as pp 
-from scipy.stats import pearsonr 
+from sklearn import preprocessing as pp
+from scipy.stats import pearsonr
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold 
-from sklearn.metrics import log_loss 
-from sklearn.metrics import precision_recall_curve, average_precision_score 
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_recall_curve, average_precision_score
 from sklearn.metrics import roc_curve, auc, roc_auc_score
-from sklearn.metrics import confusion_matrix, classification_report 
+from sklearn.metrics import confusion_matrix, classification_report
 
 '''Algos'''
 from sklearn.linear_model import LogisticRegression
@@ -39,36 +41,92 @@ data = pd.read_csv(current_path + file)
 
 # 標準化
 dataX = data.copy().drop(['Class'],axis=1)
-dataY = data['Class'].copy() 
+dataY = data['Class'].copy()
 featuresToScale = dataX.drop(['Time'],axis=1).columns
-sX = pp.StandardScaler(copy=True, with_mean=True, with_std=True) 
+sX = pp.StandardScaler(copy=True, with_mean=True, with_std=True)
 dataX.loc[:,featuresToScale] = sX.fit_transform(dataX[featuresToScale])
 
 # 訓練セットの作成
 X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.33, random_state=2018, stratify=dataY)
 
 # k分割交差検証セットの作成
-k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018) 
+k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
+
+'''Main'''
+import numpy as np
+import pandas as pd
+import os
+
+'''Data Viz'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+import matplotlib as mpl
+
+# jupyter notebookで使う
+# %matplotlib inline
+
+'''Data Prep'''
+from sklearn import preprocessing as pp
+from scipy.stats import pearsonr
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.metrics import confusion_matrix, classification_report
+
+'''Algos'''
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+import lightgbm as lgb
+
+
+# Data Preparation
+
+# データの読み込み
+current_path = os.getcwd()
+file = os.path.sep.join(['', 'datasets', 'credit_card_data', 'credit_card.csv'])
+data = pd.read_csv(current_path + file)
+#------------------------------------------------------------------------------------------
+
+# 標準化
+dataX = data.copy().drop(['Class'],axis=1)
+dataY = data['Class'].copy()
+featuresToScale = dataX.drop(['Time'],axis=1).columns
+sX = pp.StandardScaler(copy=True, with_mean=True, with_std=True)
+dataX.loc[:,featuresToScale] = sX.fit_transform(dataX[featuresToScale])
+
+# 訓練セットの作成
+X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.33, random_state=2018, stratify=dataY)
+
+# k分割交差検証セットの作成
+k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
+
 
 # ハイパーパラメータの設定 全てデフォルト
-params_xGB = {
-    'nthread':3, #number of cores
-    'learning rate': 0.3, #range 0 to 1, default 0.3
-    'gamma': 0, #range 0 to infinity, default 0 
-        # increase to reduce complexity (increase bias, reduce variance)
-    'max_depth': 6, #range 1 to infinity, default 6
-    'min_child_weight': 1, #range 0 to infinity, default 1
-    'max_delta_step': 0, #range 0 to infinity, default 0
-    'subsample': 1.0, #range 0 to 1, default 1
-        # subsample ratio of the training examples
-    'colsample_bytree': 1.0, #range 0 to 1, default 1 
-        # subsample ratio of features
-    'objective':'binary:logistic',
+params_lightGB = {
+    'task': 'train',
+    'application':'binary',
     'num_class':1,
-    'eval_metric':'logloss',
-    'seed':2018,
-    'silent':1
+    'boosting': 'gbdt',
+    'objective': 'binary',
+    'metric': 'binary_logloss',
+    'metric_freq':50,
+    'is_training_metric':False,
+    'max_depth':4,
+    'num_leaves': 31,
+    'learning_rate': 0.01,
+    'feature_fraction': 1.0,
+    'bagging_fraction': 1.0,
+    'bagging_freq': 0,
+    'bagging_seed': 2018,
+    'verbose': 0,
+    'num_threads':3
 }
+
+# モデルの訓練
 trainingScores = []
 cvScores = []
 predictionsBasedOnKFolds = pd.DataFrame(data=[],index=y_train.index,columns=['prediction'])
@@ -76,35 +134,29 @@ predictionsBasedOnKFolds = pd.DataFrame(data=[],index=y_train.index,columns=['pr
 for train_index, cv_index in k_fold.split(np.zeros(len(X_train)),y_train.ravel()):
     X_train_fold, X_cv_fold = X_train.iloc[train_index,:], X_train.iloc[cv_index,:]
     y_train_fold, y_cv_fold = y_train.iloc[train_index], y_train.iloc[cv_index]
-    
-    dtrain = xgb.DMatrix(data=X_train_fold, label=y_train_fold)
-    dCV = xgb.DMatrix(data=X_cv_fold)
-    
-    bst = xgb.cv(params_xGB, dtrain, num_boost_round=2000, nfold=5, early_stopping_rounds=200, verbose_eval=50)
-    
-    best_rounds = np.argmin(bst['test-logloss-mean'])
-    bst = xgb.train(params_xGB, dtrain, best_rounds)
-    
-    loglossTraining = log_loss(y_train_fold, bst.predict(dtrain))
+
+    lgb_train = lgb.Dataset(X_train_fold, y_train_fold)
+    lgb_eval = lgb.Dataset(X_cv_fold, y_cv_fold, reference=lgb_train)
+    gbm = lgb.train(params_lightGB, lgb_train, num_boost_round=2000,valid_sets=lgb_eval, early_stopping_rounds=200)
+
+    loglossTraining = log_loss(y_train_fold, gbm.predict(X_train_fold, num_iteration=gbm.best_iteration))
     trainingScores.append(loglossTraining)
-    
-    predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'] = bst.predict(dCV)
+
+    predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'] = gbm.predict(X_cv_fold, num_iteration=gbm.best_iteration)
     loglossCV = log_loss(y_cv_fold, predictionsBasedOnKFolds.loc[X_cv_fold.index,'prediction'])
     cvScores.append(loglossCV)
-    
+
     print('Training Log Loss: ', loglossTraining)
     print('CV Log Loss: ', loglossCV)
-    
-loglossXGBoostGradientBoosting = log_loss(y_train, predictionsBasedOnKFolds.loc[:,'prediction'])
-print('XGBoost Gradient Boosting Log Loss: ', loglossXGBoostGradientBoosting)
-# 対数損失がロジスティックの1/15, ランダムフォレストの1/5になっている.
+
+loglossLightGBMGradientBoosting = log_loss(y_train, predictionsBasedOnKFolds.loc[:,'prediction'])
+print('LightGBM Gradient Boosting Log Loss: ', loglossLightGBMGradientBoosting)
 #------------------------------------------------------------------------------------------
 # 結果の評価
-
 # 平均適合率
 preds = pd.concat([y_train,predictionsBasedOnKFolds.loc[:,'prediction']], axis=1)
 preds.columns = ['trueLabel','prediction']
-predictionsBasedOnKFoldsXGBoostGradientBoosting = preds.copy()
+predictionsBasedOnKFoldsLightGBMGradientBoosting = preds.copy()
 
 precision, recall, thresholds = precision_recall_curve(preds['trueLabel'],preds['prediction']) # ある閾値の時の適合率、再現率の値を取得
 average_precision = average_precision_score(preds['trueLabel'],preds['prediction'])
@@ -119,7 +171,6 @@ plt.ylim([0.0, 1.05])
 plt.xlim([0.0, 1.0])
 
 plt.title('Precision-Recall curve: Average Precision = {0:0.2f}'.format(average_precision))
-# 平均適合率82%でロジスティックやランダムフォレストよりいい.
 
 # acROCの計算(受信者動作特性曲線下の面積) 偽陽性を可能な限り低く保ちながら不正をどれだけ捉えれるか.
 fpr, tpr, thresholds = roc_curve(preds['trueLabel'],preds['prediction'])
@@ -135,6 +186,4 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic: Area under the curve = {0:0.2f}'.format(areaUnderROC))
 plt.legend(loc="lower right")
 plt.show()
-# auROCは0.97で3つの中で最もよい.
-
-#3つの中ではXGBoostが最もよい.
+# XGBoostとほぼ同じ結果なのに速度が1459s->134sに減ってすごい
