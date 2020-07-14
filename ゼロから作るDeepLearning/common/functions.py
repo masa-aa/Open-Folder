12a17ebafe81ebd4a95bc194c2ded5b79ce74f9b
masa-aa <60830652+masa-aa@users.noreply.github.com>
2020-07-14 12:48:27 +0900

    4章終了

--- ゼロから作るDeepLearning/common/functions.py
+++ ゼロから作るDeepLearning/common/functions.py
@@ -24,3 +24,18 @@ def softmax(x):
 
 def mean_squared_errpr(y, t):
     return 0.5 * np.sum((y - t)** 2)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return - np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
