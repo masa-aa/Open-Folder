3fa3e7cf90ffccd245397e36508e91f379321ec8
masa-aa <masayayamaguchi480@gmail.com>
2020-08-17 01:25:44 +0900

    7章完

--- ゼロから作るDeepLearning/common/util.py
+++ ゼロから作るDeepLearning/common/util.py
@@ -45,20 +45,22 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     -------
     col : 2次元配列
     """
-    N, C, H, W = input_data.shape
+    N, C, H, W = input_data.shape # N: 個数, C: チャンネル数（色）
     out_h = (H + 2*pad - filter_h)//stride + 1
     out_w = (W + 2*pad - filter_w)//stride + 1
 
-    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant') # パディング
     col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
     for y in range(filter_h):
         y_max = y + stride*out_h
         for x in range(filter_w):
-            x_max = x + stride*out_w
+            x_max = x + stride * out_w
+            # yからy_maxまでy+strideする
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
-    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
     return col
 
 
@@ -85,6 +87,6 @@ def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
         y_max = y + stride*out_h
         for x in range(filter_w):
             x_max = x + stride*out_w
-            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+            img[:,:, y:y_max:stride, x:x_max:stride] += col[:,:, y, x,:,:]
 
     return img[:, :, pad:H + pad, pad:W + pad]
