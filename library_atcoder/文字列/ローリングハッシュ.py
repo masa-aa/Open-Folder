34d3b2d241a933a85e7fba8b4d7d9e6aed15679b
masa-aa <masayayamaguchi480@gmail.com>
2021-01-11 16:03:44 +0900

    Update ローリングハッシュ.py

--- library_atcoder/文字列/ローリングハッシュ.py
+++ library_atcoder/文字列/ローリングハッシュ.py
@@ -12,41 +12,58 @@ class RollingHash:
                      6812, 7715, 5006, 6068, 6340, 4989, 5510, 6380, 1200, 6739,
                      5527, 4000, 6519, 3448, 2933, 6048, 3133, 1667, 9086, 8368,
                      4914, 7142, 2770, 7752, 391, 7052, 5476, 3105, 8322, 3501,
-                     7454, 3167, 8730, 9002, 4564, 138, 2197, 7238, 3411, 7433][randint(0, 100)]
-        self.mod = 4611686018427387903
+                     7454, 3167, 8730, 9002, 4564, 138, 2197, 7238, 3411, 7433][randint(0, 99)]
+        self.mod = 2305843009213693951  # 2**61 - 1
         self.size = len(s)
         self.string = s
 
-        self.hash = self.make_hashtable(s)
-        self.pow = self.make_powtable()
+        self.hash = self.__make_hashtable(s)
+        self.pow = self.__make_powtable()
 
-    def make_hashtable(self, _s):
+    def __make_hashtable(self, _s):
         hashtable = [0] * (self.size + 1)
         for i in range(self.size):
             hashtable[i + 1] = (hashtable[i] * self.base + ord(_s[i])) % self.mod
         return hashtable
 
-    def make_powtable(self):
+    def __make_powtable(self):
         power = [1] * (self.size + 1)
         for i in range(self.size):
             power[i + 1] = (self.base * power[i]) % self.mod
         return power
 
-    def get_hash(self, left, right):
+    def __get_hash(self, left, right):
         """get hash of s[left:right]"""
         return (self.hash[right] - self.hash[left] * self.pow[right - left]) % self.mod
 
     def contain(self, a):
+        """return all s.index(a)"""
+        m = len(a)
+        hasha = 0
+        index = []
+        if m > self.size:
+            return index
+        for i in range(m):
+            hasha = (hasha * self.base + ord(a[i])) % self.mod
+        for i in range(self.size - m + 1):
+            hashs = self.__get_hash(i, m + i)
+            if hasha == hashs:
+                index.append(i)
+        return index
+
+    def is_contain(self, a):
         """return a in s"""
         m = len(a)
         if m > self.size:
             return False
-        hashs = self.get_hash(0, m)
         hasha = 0
         for i in range(m):
             hasha = (hasha * self.base + ord(a[i])) % self.mod
         for i in range(self.size - m + 1):
+            hashs = self.__get_hash(i, m + i)
             if hasha == hashs:
                 return True
-            hashs = self.get_hash(i, m + i)
         return hasha == hashs
+
+# verify contain
+# https://onlinejudge.u-aizu.ac.jp/solutions/problem/ALDS1_14_B/review/5127891/masa_aa/Python3
